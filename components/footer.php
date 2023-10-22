df8f87e3e18da0b6b11610d8f63c6a3276e9a0c0
Deewakar2002Jha <deewakarjha0541@gmail.com>
2023-10-22 14:35:14 +0530

    Gobal knowledge

---
+++ components/footer.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+
+   &copy; copyright<?= date('Y'); ?> Patna <span>Gobal knowledge
+</span> | Pvt Liminted
+<!-- (<?= date('Y'); ?>) this show the current lisensce valid till-->
+
+</footer>
